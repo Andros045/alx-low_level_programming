d06f78529da3720b787887ab3188bbbf9c5a0766
Andros045 <balogunandros@gmail.com>
2022-04-07 21:54:27 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The containing fumction of this programme
+ * Return: this gives 0 if successfuk
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

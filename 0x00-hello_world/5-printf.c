8d2abbb255f65c9cd5626c622ef792608e825c8e
Andros045 <balogunandros@gmail.com>
2022-04-07 21:59:33 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - the containing fun
+ * Return: 0 if sucessful
+ */
+/int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}

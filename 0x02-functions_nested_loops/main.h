3b7357d690e3531566b39fac50c6b3151ec4e749
Andros045 <balogunandros@gmail.com>
2022-04-12 16:52:32 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 /**
  * _putchar - writes the character c to stdout
  * print_alphabet - writes the letters of the alphabet to stdout
+ * print_alphabet_x10 - writes the alphabet 10 times
  */
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);

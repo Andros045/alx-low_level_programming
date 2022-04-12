7c6394f3b9d6fde77c4c56d3fe1ce7cd7e5f169a
Andros045 <balogunandros@gmail.com>
2022-04-12 16:56:40 -0700

    3-islower.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,9 @@
-/**
  * _putchar - writes the character c to stdout
  * print_alphabet - writes the letters of the alphabet to stdout
  * print_alphabet_x10 - writes the alphabet 10 times
+ * _islower - check whether the character is lowercase or not.
  */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);

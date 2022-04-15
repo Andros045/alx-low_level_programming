79a3d938da5206e2187fa3e6f77e01ec3d152dc2
Andros045 <balogunandros@gmail.com>
2022-04-15 13:46:31 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,15 +3,7 @@
  * _putchar - writes the character c to stdout
  * _isupper - checks if char is uppercase
  * _isdigit - checkd if c is a digit
- * mul - multiplies two numbers and return the result
- * print_numbers - print numbers from 0 - 9 to stdout
- * print_most_numbers - print 0-9 except 2 & 4
- * more_numbers - print 1 - 14 to stdout 10 times
- * print_line - print lines nth times
- * print_diagonal - print diagonal lines
- * print_square - print squares according to size
- * print_triangle -print triangles according to size
- * print_number - prints an integer
  */
 int _putchar(char c);
 int _isupper(int c);
+int _isdigit(int c);
